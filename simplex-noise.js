7646ba846f1a54c374d30b373fbde40edb9c79df
Jonas Wagner <jonas@29a.ch>
2016-12-31 01:30:01 +0100

    Revert coefficients, use permutation table

--- simplex-noise.js
+++ simplex-noise.js
@@ -44,10 +44,7 @@ function SimplexNoise(random) {
   this.p = buildPermutationTable(random);
   this.perm = new Uint8Array(512);
   this.permMod12 = new Uint8Array(512);
-  for (var i = 0; i < 256; i++) {
-    this.p[i] = random() * 256;
-  }
-  for (i = 0; i < 512; i++) {
+  for (var i = 0; i < 512; i++) {
     this.perm[i] = this.p[i & 255];
     this.permMod12[i] = this.perm[i] % 12;
   }
@@ -229,28 +226,28 @@ SimplexNoise.prototype = {
         var jj = j & 255;
         var kk = k & 255;
         // Calculate the contribution from the four corners
-        var t0 = 0.5 - x0 * x0 - y0 * y0 - z0 * z0;
+        var t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
         if (t0 < 0) n0 = 0.0;
         else {
           var gi0 = permMod12[ii + perm[jj + perm[kk]]] * 3;
           t0 *= t0;
           n0 = t0 * t0 * (grad3[gi0] * x0 + grad3[gi0 + 1] * y0 + grad3[gi0 + 2] * z0);
         }
-        var t1 = 0.5 - x1 * x1 - y1 * y1 - z1 * z1;
+        var t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1;
         if (t1 < 0) n1 = 0.0;
         else {
           var gi1 = permMod12[ii + i1 + perm[jj + j1 + perm[kk + k1]]] * 3;
           t1 *= t1;
           n1 = t1 * t1 * (grad3[gi1] * x1 + grad3[gi1 + 1] * y1 + grad3[gi1 + 2] * z1);
         }
-        var t2 = 0.5 - x2 * x2 - y2 * y2 - z2 * z2;
+        var t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2;
         if (t2 < 0) n2 = 0.0;
         else {
           var gi2 = permMod12[ii + i2 + perm[jj + j2 + perm[kk + k2]]] * 3;
           t2 *= t2;
           n2 = t2 * t2 * (grad3[gi2] * x2 + grad3[gi2 + 1] * y2 + grad3[gi2 + 2] * z2);
         }
-        var t3 = 0.5 - x3 * x3 - y3 * y3 - z3 * z3;
+        var t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3;
         if (t3 < 0) n3 = 0.0;
         else {
           var gi3 = permMod12[ii + 1 + perm[jj + 1 + perm[kk + 1]]] * 3;
@@ -349,35 +346,35 @@ SimplexNoise.prototype = {
         var kk = k & 255;
         var ll = l & 255;
         // Calculate the contribution from the five corners
-        var t0 = 0.5 - x0 * x0 - y0 * y0 - z0 * z0 - w0 * w0;
+        var t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0 - w0 * w0;
         if (t0 < 0) n0 = 0.0;
         else {
           var gi0 = (perm[ii + perm[jj + perm[kk + perm[ll]]]] % 32) * 4;
           t0 *= t0;
           n0 = t0 * t0 * (grad4[gi0] * x0 + grad4[gi0 + 1] * y0 + grad4[gi0 + 2] * z0 + grad4[gi0 + 3] * w0);
         }
-        var t1 = 0.5 - x1 * x1 - y1 * y1 - z1 * z1 - w1 * w1;
+        var t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1 - w1 * w1;
         if (t1 < 0) n1 = 0.0;
         else {
           var gi1 = (perm[ii + i1 + perm[jj + j1 + perm[kk + k1 + perm[ll + l1]]]] % 32) * 4;
           t1 *= t1;
           n1 = t1 * t1 * (grad4[gi1] * x1 + grad4[gi1 + 1] * y1 + grad4[gi1 + 2] * z1 + grad4[gi1 + 3] * w1);
         }
-        var t2 = 0.5 - x2 * x2 - y2 * y2 - z2 * z2 - w2 * w2;
+        var t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2 - w2 * w2;
         if (t2 < 0) n2 = 0.0;
         else {
           var gi2 = (perm[ii + i2 + perm[jj + j2 + perm[kk + k2 + perm[ll + l2]]]] % 32) * 4;
           t2 *= t2;
           n2 = t2 * t2 * (grad4[gi2] * x2 + grad4[gi2 + 1] * y2 + grad4[gi2 + 2] * z2 + grad4[gi2 + 3] * w2);
         }
-        var t3 = 0.5 - x3 * x3 - y3 * y3 - z3 * z3 - w3 * w3;
+        var t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3 - w3 * w3;
         if (t3 < 0) n3 = 0.0;
         else {
           var gi3 = (perm[ii + i3 + perm[jj + j3 + perm[kk + k3 + perm[ll + l3]]]] % 32) * 4;
           t3 *= t3;
           n3 = t3 * t3 * (grad4[gi3] * x3 + grad4[gi3 + 1] * y3 + grad4[gi3 + 2] * z3 + grad4[gi3 + 3] * w3);
         }
-        var t4 = 0.5 - x4 * x4 - y4 * y4 - z4 * z4 - w4 * w4;
+        var t4 = 0.6 - x4 * x4 - y4 * y4 - z4 * z4 - w4 * w4;
         if (t4 < 0) n4 = 0.0;
         else {
           var gi4 = (perm[ii + 1 + perm[jj + 1 + perm[kk + 1 + perm[ll + 1]]]] % 32) * 4;
