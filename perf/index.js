e86e5aaf4ae1da1a676bb68a3e150f84bdce39c3
Jonas Wagner <jonas@29a.ch>
2021-08-25 11:13:11 +0200

    comparative benchmark with fast-simplex-noise

--- perf/index.js
+++ perf/index.js
@@ -5,6 +5,11 @@ let SimplexNoise = require('..').SimplexNoise;
 const invocationsPerRun = 8*8*8;
 const simplex = new SimplexNoise('seed');
 
+const {makeNoise2D,makeNoise4D} = require('fast-simplex-noise');
+const noise2D = makeNoise2D();
+const noise4D = makeNoise4D();
+
+
 // prevent the compiler from optimizing away the calls
 let sideEffect = 0.0;
 new Benchmark.Suite('simplex-noise')
@@ -17,6 +22,17 @@ new Benchmark.Suite('simplex-noise')
       for (let y = 0; y < 8; y++) {
         for (let z = 0; z < 8; z++) {
           a += simplex.noise2D(x / 8, y / 8);
+        }
+      }
+    }
+    sideEffect += a;
+  })
+  .add('fast-simplex-noise noise2D', function() {
+    let a = 0.0;
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        for (let z = 0; z < 8; z++) {
+          a += noise2D(x / 8, y / 8);
         }
       }
     }
@@ -44,6 +60,18 @@ new Benchmark.Suite('simplex-noise')
     }
     sideEffect += a;
   })
+  .add('fast-simplex-noise noise4D', function() {
+    let a = 0.0;
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        for (let z = 0; z < 8; z++) {
+          a += noise4D(x / 8, y / 8, z / 8, (x + y) / 16);
+        }
+      }
+    }
+    sideEffect += a;
+  })
+  
   .on('complete', function() {
     console.log(sideEffect);
     this.forEach(function(bench) {
