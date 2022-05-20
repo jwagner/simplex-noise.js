bf0fffca631eaefd4ab93be29b23efd34784afde
Mike <reinstein.mike@gmail.com>
2022-05-20 16:15:36 -0700

    use the alea npm module rather than embedding. fixes #31

--- simplex-noise.ts
+++ simplex-noise.ts
@@ -27,6 +27,9 @@ Better rank ordering method by Stefan Gustavson in 2012.
  SOFTWARE.
  */
 
+import alea from 'alea';
+
+
 const F2 = 0.5 * (Math.sqrt(3.0) - 1.0);
 const G2 = (3.0 - Math.sqrt(3.0)) / 6.0;
 const F3 = 1.0 / 3.0;
@@ -435,59 +438,4 @@ export function buildPermutationTable(random: RandomFn): Uint8Array {
     p[r] = aux;
   }
   return p;
-}
-
-/*
-The ALEA PRNG and masher code used by simplex-noise.js
-is based on code by Johannes Baagøe, modified by Jonas Wagner.
-See alea.md for the full license.
-*/
-function alea(seed: string|number): RandomFn {
-  let s0 = 0;
-  let s1 = 0;
-  let s2 = 0;
-  let c = 1;
-
-  const mash = masher();
-  s0 = mash(' ');
-  s1 = mash(' ');
-  s2 = mash(' ');
-
-  s0 -= mash(seed);
-  if (s0 < 0) {
-    s0 += 1;
-  }
-  s1 -= mash(seed);
-  if (s1 < 0) {
-    s1 += 1;
-  }
-  s2 -= mash(seed);
-  if (s2 < 0) {
-    s2 += 1;
-  }
-
-  return function() {
-    const t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
-    s0 = s1;
-    s1 = s2;
-    return s2 = t - (c = t | 0);
-  };
-}
-
-function masher() {
-  let n = 0xefc8249d;
-  return function(data: number|string) {
-    data = data.toString();
-    for (let i = 0; i < data.length; i++) {
-      n += data.charCodeAt(i);
-      let h = 0.02519603282416938 * n;
-      n = h >>> 0;
-      h -= n;
-      h *= n;
-      n = h >>> 0;
-      h -= n;
-      n += h * 0x100000000; // 2^32
-    }
-    return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
-  };
 }
